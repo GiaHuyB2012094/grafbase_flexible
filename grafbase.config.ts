3718842430e2fd3c23363a1a3c26898acf5ea286
Huy <huyDEVSE040902@gmail.com>
2023-12-14 15:40:41 +0700

    update

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -2,23 +2,23 @@ import { graph, config } from '@grafbase/sdk'
 const g = graph.Standalone()
 
 const User = g.type('User', {
-  name: g.string().length({min: 2, max: 20}),
-  email: g.string().unique(),
+  name: g.string(),
+  email: g.string(),
   avatarUrl: g.url(),
   description: g.string(),
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
-  projects: g.relation(()=> Project).list().optional(),
+  // projects: g.relation(()=> Project).list().optional(),
 })
 
 const Project = g.type("Project",{
-  title: g.string().length({min :3}),
+  title: g.string(),
   description: g.string(),
   image: g.url(),
   liveSiteUrl: g.url(),
   githubUrl: g.url(),
-  category: g.string().search(),
-  createBy: g.relation(()=> User),
+  category: g.string(),
+  // createBy: g.relation(()=> User),
 
 })
 export default config({
